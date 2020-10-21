7cbe29ab47ea9a39b01849be9da8e506220fad6e
Ryder Anderson <ra302211@gmail.com>
2020-10-21 14:38:23 -0500

    Added test map and map scene for server
    also adjusted spawns for player and test actor

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -39,7 +39,7 @@ public class NetworkManager : MonoBehaviour
     }
     public Player InstantiatePlayer()
     {
-        return Instantiate(playerPrefab, new Vector3(0,0.5f,0), Quaternion.identity).GetComponent<Player>();
+        return Instantiate(playerPrefab, new Vector3(0,100f,0), Quaternion.identity).GetComponent<Player>();
     }
 
     
